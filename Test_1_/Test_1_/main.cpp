cc31d136b32f4e70e0d8ea60bfd10a83e406535d
SAMODIL <ab9054837@akfauniversity.org>
2022-04-28 15:23:15 +0500

    Test_1
    it is the second file

---
+++ Test_1_/Test_1_/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Test_1_
+//
+//  Created by SAMODIL BEKNAZAROVA on 28/04/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
